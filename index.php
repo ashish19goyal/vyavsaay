704013cea3c77486203fe95a6c18811beba1c3a9
Ashish Goyal <ashish@vyavsaay.com>
2014-10-20 22:10:27 +0530

    v17.2-20141020

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 	include "includes/header.php";
 ?>
-
 		<!----- start-header---->
 			<div id="home" class="header">
 				<div class="top-header">
@@ -11,13 +10,13 @@
 						<!----start-top-nav---->
 						 <nav class="top-nav">
 							<ul class="top-nav">
-								<li class="active"><a href="#home" class="scroll">Home </a></li>
-								<li><a href="#about" class="scroll">About</a></li>
-								<li><a href="#reviews" class="scroll">Reviews</a></li>
-								<li><a href="#demo" class="scroll">Demo</a></li>
-								<li><a href="#d_register" class="scroll">Register</a></li>
-								<li><a href="#d_register" class="scroll">Login</a></li>
-								<li><a href="#contact" class="scroll">Contact</a></li>
+								<li><a href="#home" class="top-nav">Home </a></li>
+								<li><a href="#about" class="top-nav">About</a></li>
+								<li><a href="#reviews" class="top-nav">Reviews</a></li>
+								<li><a href="#demo" class="top-nav">Demo</a></li>
+								<li><a href="#d_register" class="top-nav">Register</a></li>
+								<li><a href="#d_register" class="top-nav">Login</a></li>
+								<li><a href="#contact" class="top-nav">Contact</a></li>
 							</ul>
 							<a href="#" id="pull"><img src="images/menu-icon.png" title="menu" /></a>
 						</nav>
@@ -29,12 +28,12 @@
 			<div class="banner">
 				<div class="container">
 					<div class="banner-info">
-						<div class=" col-md-6 appp">
+						<div class="col-md-6 appp">
 							 <img src="images/mbl-app.png" alt="">
 						</div>
 						<div class="col-md-6 banner-text">
 							<h1>Vyavsaay</h1>
-							<p>Vyavsaay provides a cloud based platform to manage all business processes and information.</p>
+							<p>Vyavsaay provides a cloud based, cross platform solution to manage all business processes and information.</p>
 						</div>
 						<div class="clearfix"> </div>
 					</div>
@@ -46,7 +45,7 @@
 	       		<div class="container">
 	       		   <div class="gallery-head text-center">
 					  <h3>ABOUT</h3>
-					  <p>Vyavsaay is best suited for small and medium enterprises that don't want to spend a lot of money in setting up and managing IT infrastructure. It is cross platform and supports all desktop, tablets and mobile devices. The best part is that you don't need an active internet connection to use it.</p>
+					  <p>Vyavsaay is best suited for small and medium enterprises that don't want to spend a lot of money in setting up and managing IT infrastructure. And you also don't need an active internet connection to use it.</p>
 					 <p>Vyavsaay takes care of managing your business information and automation of business processes, so that you can focus on developing client relationships and optimizing business processes for the growth of your business.</p>
 				    </div>
 	       			<div class="row text-center">
@@ -76,7 +75,7 @@
 			       					 <img src="images/img-1.jpg" alt="">
 			       				</div>
 			       				<div class="col-md-8 review-text">
-			       					<p>"Vyavsaay is very user friendly. Anybody can use it with ease."</p>
+			       					<p>"Vyavsaay is very user friendly. Anybody can use it with ease. It took us only a few hours to setup and get started with Vyavsaay."</p>
 			       					<span>Employee</span>
 			       				</div>
 			       				<div class="clearfix"> </div>
@@ -91,17 +90,6 @@
 			       				</div>
 			       				<div class="clearfix"> </div>
 			       			</div>
-			       			<div class="review_grid">
-			       				<div class="col-md-4 review-img3">
-			       					 <img src="images/img-3.png" alt="">
-			       				</div>
-			       				<div class="col-md-8 review-text">
-			       					<p>"It took us only a few hours to setup and get started with Vyavsaay"</p>
-			       					<span>Owner</span>
-			       				</div>
-			       				<div class="clearfix"> </div>
-			       			</div>
-			       			 <a class="review-btn" href="#">READ MORE</a>
 		       			</div>
 		       			<div class="clearfix"> </div>
 		       		</div>
@@ -116,19 +104,10 @@
        			<div class="row Demo text-center">
        				<div class="demo-grid">
 	       				<div class="col-md-5 demo-img1">
-	       					 <img src="web/images/ab-1.png" alt="">
+	       					 <img src="images/ab-1.png" alt="">
 	       				</div>
 	       				<div class="col-md-7 Demo-text">
 	       					<p>Here is a quick introduction video for vyavsaay</p>
-	       				</div>
-	       				<div class="clearfix"> </div>
-	       			</div>
-       				<div class="demo-grid">
-	       				<div class="col-md-5 demo-img2">
-	       					 <img src="images/ab-2.png" alt="">
-	       				</div>
-	       				<div class="col-md-7 button">
-	       					 <a class="demo-btn" href="#">View Demo</a>
 	       				</div>
 	       				<div class="clearfix"> </div>
 	       			</div>
@@ -143,7 +122,9 @@
        			<div class="Downloads">
        				<div class="download_left">
        					<h3>Register</h3>
-       					<p>Click the following link to register for a new account. The subscription is free for first 3 months.</p>
+       					<div class="Demo-text">
+       						<p>Click the following link to register for a new account. The subscription is free for first 3 months.</p>
+       					</div>
        					<a class="Down-btn" href="register.php">Register</a>
        				</div>
        				
@@ -156,7 +137,7 @@
 									Login ID: <input type="text" autocomplete="on" autofocus="autofocus" required></br>
 									Password: <input type="password" required></br>
 									<a href="pass_reset.php">Forgot password?</a></br>
-									<input type="submit" value='Submit'>
+									<input type="submit" value='Login' class="Down-btn">
 								</fieldset>
 							</form>
        				</div>
@@ -173,13 +154,13 @@
       			  <div class="contact-form">
 		  	        <form>
 			    	    <p class="comment-form-author"><label for="author"> Name</label>
-			    	    	<input type="text" class="textbox" value="Enter your name..." onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Enter your name...';}">
+			    	    	<input type="text" class="textbox" placeholder="Enter your name...">
 				    	</p>
 				        <p class="comment-form-author"><label for="author">Email</label>
-				     	   <input type="text" class="textbox" value="Enter your email..." onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Enter your email...';}">
+				     	   <input type="text" class="textbox" placeholder="Enter your email...">
 				        </p>
 				        <p class="comment-form-author"><label for="author">Message</label>
-				    	  <textarea value="Enter your message..." onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Message';}">Enter your message...</textarea>
+				    	  <textarea placeholder='Enter your message...'></textarea>
 				        </p>
 				        <input name="submit" type="submit" id="submit" value="Submit">
 			        </form>
@@ -203,6 +184,3 @@
 			login_action();
 		});
 </script>
-<?php 
-	include "includes/footer.php";
-?>
