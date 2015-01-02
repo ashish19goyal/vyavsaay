c79c325a92be5e6d7fa9ce80c71abd850adfb537
Ashish Goyal <ashish@vyavsaay.com>
2015-01-02 18:13:01 +0530

    v74.6-20150102

--- includes/header.php
+++ includes/header.php
@@ -12,34 +12,6 @@
 			include "css_includes.php";
 		?>
 		<link rel="icon" href="./images/favicon.png">
-		<script>
-
-			// Store the meta element
-			var viewport_meta = document.getElementById('viewport-meta');
-	
-			// Define our viewport meta values
-			var viewports = {
-					default: viewport_meta.getAttribute('content'),
-					landscape: 'width=1000 user-scalable=no'
-				};
-	
-			// Change the viewport value based on screen.width
-			var viewport_set = function() {
-					if ( screen.width > 768 )
-						viewport_meta.setAttribute( 'content', viewports.landscape );
-					else
-						viewport_meta.setAttribute( 'content', viewports.default );
-				}
-	
-			// Set the correct viewport value on page load
-			viewport_set();
-	
-			// Set the correct viewport after device orientation change or resize
-			window.onresize = function() { 
-				viewport_set(); 
-			}
-		
-		</script>
 		
 	</head>
 	<body onload="default_load();">
