4dad393262a77ef5bb7e22ef79f961bb12d7d7de
ashish-thinkpad <thinkpad@vyavsaay.com>
2015-03-25 18:09:50 +0530

    v121.4-20150324

--- JavaScripts/prop/event_handler.js
+++ JavaScripts/prop/event_handler.js
@@ -310,7 +310,7 @@ function hide_unreadable_elements()
 {
 	var forms_array=[];
 	var reports_array=[];
-	for (var i=1;i<141;i++)
+	for (var i=1;i<146;i++)
 	{
 		forms_array.push("form"+i);
 		reports_array.push("report"+i);
