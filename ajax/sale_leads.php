c0af329383cd96f39a2700f683ea07b3fa64f41e
ashish-thinkpad <thinkpad@vyavsaay.com>
2015-08-04 01:21:32 +0530

    sale_leads.php

--- ajax/sale_leads.php
+++ ajax/sale_leads.php
@@ -17,14 +17,26 @@
 
 	$acc_name=$name." (".$phone.")";
 	$due_time=1000*(time()+3*86400);
+
 	$sl_query="insert into sale_leads (customer,detail,due_date,last_updated) values(?,?,?,?);";
+	$cs_query="select acc_name from customers where acc_name=?";
 	$c_query="insert into customers (name,phone,email,acc_name,status,address,city,last_updated) values(?,?,?,?,?,?,?,?);";
-	$sl_stmt=$conn->conn->prepare($sl_query);
-	$c_stmt=$conn->conn->prepare($c_query);
+	$ca_query="insert into attributes (name,type,attribute,value,last_updated) values(?,?,?,?,?);";
 	
-	$c_stmt->execute(array($name,$phone,$email,$acc_name,'active',$address,$city,(1000*time())));
+	$cs_stmt=$conn->conn->prepare($cs_query);
+	
+	$cs_result=$cs_stmt->execute(array($acc_name));
+	$struct_res=$cs_stmt->fetchAll(PDO::FETCH_NUM);
+	if(count($struct_res)==0)
+	{		
+		$c_stmt=$conn->conn->prepare($c_query);
+		$c_stmt->execute(array($name,$phone,$email,$acc_name,'active',$address,$city,(1000*time())));
+		$ca_stmt=$conn->conn->prepare($ca_query);
+		$ca_stmt->execute(array($acc_name,'customer','Company',$company,(1000*time())));
+	}	
+
+	$sl_stmt=$conn->conn->prepare($sl_query);
 	$sl_stmt->execute(array($acc_name,$requirement,$due_time,(1000*time())));
 					
 	echo "contact saved";
-
 ?>
