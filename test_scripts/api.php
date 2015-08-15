03b7a5ee7fc1896da89627cd7b360e394cb9699e
ashish-thinkpad <thinkpad@vyavsaay.com>
2015-08-16 02:46:16 +0530

    beacon api integration- try3

--- test_scripts/api.php
+++ test_scripts/api.php
@@ -2,8 +2,8 @@
 
 	$awb_num=$_POST['awb_num'];
 	$data_object=[];
-	$data_object['api_key']="becontent15082015";
-	$data_object['username']="becontent";
+	$data_object['api_key']="12345";
+	$data_object['username']="vyavsaay";
 	$data_object['data_store']="logistics_orders";
 	$data_object['count']="1";
 	$data_object['start_index']="0";
@@ -15,8 +15,8 @@
 		
     $data_string="data=".json_encode($data_object);
 	
-	//$url="localhost/api/get_data.php";
-	$url="https://vyavsaay.com/api/get_data.php";
+	$url="localhost/api/get_data.php";
+	//$url="https://vyavsaay.com/api/get_data.php";
 
 	$ch=curl_init();
 	curl_setopt($ch,CURLOPT_URL,$url);
@@ -24,6 +24,7 @@
     curl_setopt($ch,CURLOPT_POSTFIELDS,$data_string);
     curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
 	curl_setopt($ch,CURLOPT_VERBOSE,true);
+	curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,0);
 	//$verbose = fopen('php://temp', 'w+');
 	//curl_setopt($ch, CURLOPT_STDERR, $verbose);
 	$result = curl_exec($ch);
