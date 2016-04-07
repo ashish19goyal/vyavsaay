2df55fd409e2ebd0921965321bf934410c052741
ashish-thinkpad <ashish.19goyal@gmail.com>
2016-04-08 00:30:43 +0530

    object pages

---
+++ new/objects/product_master/inventory.php
@@ -0,0 +1,18 @@
+<div class="profile-userpic">
+	<div class="profile-usertitle">
+        <div class="profile-usertitle-name" style='text-align:left;'> <span>Quantity </span> <span class='label label-success' id='object_product_master_inventory'>0</span> </div>
+	</div>
+	<script>
+		function initialize_object_product_master_inventory(obj_name,obj_id)
+		{
+            var container=document.getElementById('object_product_master_inventory_body');
+            container.innerHTML="";
+            
+            get_inventory(obj_name,'',function(inventory)
+            {
+                $('#object_product_master_inventory').html(inventory);
+            });    
+		} 
+
+	</script>
+</div>
