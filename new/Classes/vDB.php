b4cfa4bc174ee3b1d514d273cfd0d267def8433b
Ashish Goyal <ashish.19goyal@gmail.com>
2016-07-07 01:08:23 +0530

    Automated commit on 2016-07-07 01:08:23 IST

--- new/Classes/vDB.php
+++ new/Classes/vDB.php
@@ -8,7 +8,10 @@ include_once 'config.php';
 class vDB
 {
 	private $conn;
-
+	private $table;
+	/**
+	* Constructor initializes a connnection to the database
+	*/
 	public function __construct($db_name)
 	{
 		$config = config::getInstance();
@@ -26,7 +29,19 @@ class vDB
 		$this->conn = new \PDO($dsn, $dbuser, $dbpass, $options);
 	}
 
+	/**
+	* Destructor releases the connnection to the database
+	*/
+	public function __destruct()
+	{
+		unset($this->conn);
+	}
+
 	//db level functions
+
+	/**
+	* Performs a simple select query
+	*/
 	public function dbSelect($query,$values)
 	{
 		$stmt=$this->conn->prepare($query);
@@ -34,56 +49,401 @@ class vDB
 		return $stmt->fetchAll(PDO::FETCH_ASSOC);
 	}
 
+	/**
+	* Executes a simple insert,update or delete query
+	*/
 	public function dbExecute($query,$values)
 	{
 		$stmt=$this->conn->prepare($query);
 		return $stmt->execute($values);
 	}
 
+
 	//generic functions in the context of vyavsaay
-	public function vDelete($table,$indexes)
+
+	/**
+	* Performs delete as per the provided indexes
+	*/
+	public function vDelete($indexes)
 	{
 		$whereArray=$this->getWhereClause($indexes);
-		$query=$this->getQuery($table,'delete','',$whereArray['query']);
+
+		$subQueries = array(
+				'where' => $whereArray['query']
+		);
+		$query=$this->getQuery('delete',$subQueries);
+
+		$query=$this->getQuery('delete','',$whereArray['query']);
 		return $this->dbExecute($query,$whereArray['values']);
 	}
 
-	public function vUpdate($table,$indexes,$data)
+	/**
+	* Performs update as per the provided indexes and data
+	*/
+	public function vUpdate($indexes,$data)
 	{
 		$setArray=$this->getSetClause($data);
 		$whereArray=$this->getWhereClause($indexes);
-		$query=$this->getQuery($table,'update',$setArray['query'],$whereArray['query']);
+
+		$subQueries = array(
+				'where' => $whereArray['query'],
+				'set' => $setArray['query']
+		);
+		$query=$this->getQuery('update',$subQueries);
+
 		$values=array_merge($setArray['values'],$whereArray['values']);
 		return $this->dbExecute($query,$values);
 	}
 
-	public function vCreate($table,$data)
+	/**
+	* Creates a new record as per the provided data
+	*/
+	public function vCreate($data)
 	{
 		$valuesArray=$this->getValuesClause($data);
-		$query=$this->getQuery($table,'create',$valuesArray['query']);
+
+		$subQueries = array(
+				'values' => $valuesArray['query']
+		);
+		$query=$this->getQuery('insert',$subQueries);
+
 		return $this->dbExecute($query,$valuesArray['values']);
 	}
 
-	public function vRead($table,$indexes)
+	/**
+	* Returns the selection output as per the provided indexes
+	*/
+	public function vRead($indexes,$options)
 	{
 		$whereArray=$this->getWhereClause($indexes);
-		$query=$this->getQuery($table,'read','',$whereArray['query']);
-		return $this->dbSelect($query,$whereArray['values']);
+		$limitArray=$this->getLimitClause($options);
+		$indexesClause=$this->getIndexesClause($indexes,$options);
+
+		$subQueries = array(
+				'where' => $whereArray['query'],
+				'limit' => $limitArray['query'],
+				'index' => $indexesClause
+		);
+		$query=$this->getQuery('select',$subQueries);
+		$values = array_merge($whereArray['values'],$limitArray['values']);
+		
+		$rows = $this->dbSelect($query,$values);
+
+		$startIndex = isset($options['startIndex']) ? $options['startIndex'] : 0;
+		$count = count($rows);
+		$endIndex = $startIndex + $count;
+
+		$result = array(
+			"count" => $count,
+			"endIndex" => $endIndex,
+			"rows" => array()
+		);
+
+		foreach($rows as $row)
+		{
+			$result['rows'][]=$row;
+		}
+
+		return $result;
 	}
 
-	public function vPut($table,$data)
+	/**
+	* Puts data if it doesn't already exist
+	*/
+	public function vPut($data)
 	{
-		$result=$this->vCreate($table,$data);
+		$result=$this->vCreate($data);
 		if(!$result)
 		{
-			$result=$this->vUpdate($table,array(),$data);
+			$result=$this->vUpdate(array(),$data);
 		}
 		return $result;
 	}
 
-	public function __destruct()
+	//helper functions to generate queries
+
+	/**
+	* Sets the table of the object to execute the queries
+	*/
+	public function setTable($table)
 	{
-		unset($this->conn);
+		$this->table = $table;
 	}
+
+	/**
+	* Sets the table of the object to execute the queries
+	*/
+	public function getIndexesClause($indexes,$options)
+	{
+		$all_indexes = isset($options['allIndexes']) ? $options['allIndexes'] : 'no';
+		if($all_indexes=='yes'|| count($indexes)==0)
+		{
+			return "*";
+		}
+		else{
+			$query = "";
+			foreach($indexes as $index)
+			{
+				$query.=$index['index'].",";
+			}
+			$query=rtrim($query,",");
+			return $query;
+		}
+	}
+
+	/**
+	* Generates the where clause of the query, based on the indexes
+	*/
+	private function getWhereClause($indexes)
+	{
+		$result = array(
+			'query' => '',
+			'values' => array()
+		);
+		foreach($indexes as $index)
+		{
+			if(isset($index['upperbound']))
+			{
+				$result['query'].=$this->table.".".$index['index']." <= ? and ";
+				$result['values'][]=$index['upperbound'];
+			}
+
+			if(isset($index['lowerbound']))
+			{
+				$result['query'].=$this->table.".".$index['index']." >= ? and ";
+				$result['values'][]=$index['lowerbound'];
+			}
+
+			if(isset($index['unequal']))
+			{
+				$result['query'].="(".$this->table.".".$index['index']." <> ? or isNull(".$this->table.".".$index['index'].")) and ";
+				$result['values'][]=$index['unequal'];
+			}
+
+			if(isset($index['isnull']))
+			{
+				if($index['isnull']=='yes')
+				{
+					$result['query'].="isNull(".$this->table.".".$index['index'].") and ";
+				}
+				else
+				{
+					$result['query'].="!isNull(".$this->table.".".$index['index'].") and ";
+				}
+			}
+
+			if(isset($index['array']))
+			{
+				$result['query'].=$this->table.".".$index['index']." in (";
+				$exploded_values=(array)$index['array'];
+				foreach($exploded_values as $value)
+				{
+					$result['query'].="?,";
+					$result['values'][]=$value;
+				}
+				if(count($exploded_values)==0)
+				{
+					$result['query'].="?,";
+					$result['values'][]="--";
+				}
+
+				$result['query']=rtrim($result['query'],",");
+				$result['query'].=") and ";
+			}
+
+			if(isset($index['approx_array']))
+			{
+				$approx_array=(array)$index['approx_array'];
+				$exploded_values=[];
+				foreach ($approx_array as $val)
+				{
+					$exploded_values[] = "%".$val."%";
+				}
+				$result['query'].="(";
+				foreach($exploded_values as $value)
+				{
+					$result['query'].=$this->table.".".$index['index']." like ? or ";
+					$result['values'][]=$value;
+				}
+				if(count($exploded_values)==0)
+				{
+					$result['query'].="?,";
+					$result['values'][]="--";
+				}
+
+				$result['query']=rtrim($result['query'],", or ");
+				$result['query'].=") and ";
+			}
+
+			if(isset($index['all_approx_array']))
+			{
+				$approx_array=(array)$index['all_approx_array'];
+				$exploded_values=[];
+				foreach ($approx_array as $val)
+				{
+					$exploded_values[] = "%".$val."%";
+				}
+				$result['query'].="(";
+				foreach($exploded_values as $value)
+				{
+					$result['query'].=$this->table.".".$index['index']." like ? and ";
+					$result['values'][]=$value;
+				}
+				if(count($exploded_values)==0)
+				{
+					$result['query'].="?,";
+					$result['values'][]="--";
+				}
+
+				$result['query']=rtrim($result['query'],", and ");
+				$result['query'].=") and ";
+			}
+
+			if(isset($col['value']))
+			{
+				if($col['value']!="")
+				{
+					if($col['index']=='id')
+					{
+						$result['query'].=$this->table.".".$index['index']." = ? and ";
+						$result['values'][]=$index['value'];
+					}
+					else
+					{
+						$result['query'].=$this->table.".".$index['index']." like ? and ";
+						$result['values'][]="%".$index['value']."%";
+					}
+				}
+			}
+
+			if(isset($index['exact']))
+			{
+				$result['query'].=$this->table.".".$index['index']." = ? and ";
+				$result['values'][]=$index['exact'];
+			}
+		}
+
+		$result['query']=rtrim($result['query'],"and ");
+
+		return $result;
+	}
+
+
+	/**
+	* Generates the set clause of the query, based on the data
+	*/
+	private function getSetClause($data)
+	{
+		$result = array(
+			'query' => '',
+			'values' => array()
+		);
+
+		foreach($data as $index)
+		{
+			$result['query'].=$this->table.".".$index['index']." = ?,";
+			$result['values'][]=$index['value'];
+		}
+		$result['query']=rtrim($result['query'],",");
+
+		return $result;
+	}
+
+	/**
+	* Generates the values clause of the query, based on the data
+	*/
+	private function getValuesClause($data)
+	{
+		$result = array(
+			'query' => '',
+			'values' => array()
+		);
+
+		$insert = "";
+		$values = "";
+
+		foreach($data as $index)
+		{
+			$insert.=$this->table.".".$index['index'].",";
+			$values.="?,";
+			$result['values'][]=$index['value'];
+		}
+
+		$insert=rtrim($insert,",");
+		$values=rtrim($values,",");
+
+		$result['query']="(".$insert.") values(".$values.")";
+
+		return $result;
+	}
+
+	/**
+	* Generates the Limit clause of the query, based on the data
+	*/
+	private function getLimitClause($options)
+	{
+		$result = array(
+			'query' => '',
+			'values' => array()
+		);
+		$startIndex = isset($options['startIndex']) ? $options['startIndex'] : 0;
+		$count = isset($options['count']) ? $options['count'] : 500;
+
+		if($count>5000)
+		{
+			$count=5000;
+		}
+
+		$result['query'] = "limit ?,?";
+		$result['values'][] = $startIndex;
+		$result['values'][] = $count;
+
+		return $result;
+	}
+
+	/**
+	* Generates the Complete query, from subqueries
+	*/
+	private function getQuery($type,$subQueries)
+	{
+		$index = isset($subQueries['index'])? $subQueries['index'] : "*";
+		$where = isset($subQueries['where'])? $subQueries['where'] : "";
+		$set = isset($subQueries['set'])? $subQueries['set'] : "";
+		$values = isset($subQueries['values'])? $subQueries['values'] : "";
+		$limit = isset($subQueries['limit'])? $subQueries['limit'] : "";
+
+		switch($type)
+		{
+			case 'select': if($where!="")
+							{
+								$query = "select " . $index . " from " . $this->table . " where " . $where . " " . $limit. ";";
+							}
+							else{
+								$query = "select " . $index . " from " . $this->table . " " . $limit. ";";
+							}
+							return $query;
+			case 'insert': if($values!="")
+							{
+								$query = "insert into " . $this->table . " " . $values. ";";
+								return $query;
+							}
+							break;
+			case 'update': if($where!="")
+							{
+								$query = "update " . $this->table . " set " . $set . " where " . $where. ";";
+								return $query;
+							}
+							break;
+			case 'delete': if($where!="")
+							{
+								$query = "delete from " . $this->table . " where " . $where. ";";
+								return $query;
+							}
+							break;
+		}
+
+		return "";
+	}
+
 }
 ?>
---
+++ new/ajax/update_master_db_schema.php
@@ -0,0 +1,83 @@
+<?php
+
+	include_once "../Classes/db.php";
+	use RetailingEssentials\db_connect;
+
+	function update_tables($db_name,$info_conn,$db_schema)
+	{
+		$db_conn=new db_connect($db_name);
+
+		foreach($db_schema->childNodes as $table)
+		{
+			$table_name=$table->nodeName;
+
+			if($table_name!='#text' && $table_name!='#comment')
+			{
+				///get the schema of the table
+				$get_query="select column_name,data_type from columns where table_schema=? and table_name=?";
+				$get_stmt=$info_conn->conn->prepare($get_query);
+				$get_stmt->execute(array($db_name,$table_name));
+				$get_res=$get_stmt->fetchAll(PDO::FETCH_ASSOC);
+
+				if(count($get_res)==0)
+				{
+					$q_string="create table $table_name (";
+
+					foreach($table->childNodes as $column)
+					{
+						if($column->nodeName!='#text' && $column->nodeName!='#comment')
+							$q_string.=$column->nodeName." ".$column->getAttribute('type')." ,";
+					}
+					$q_string.="PRIMARY KEY (id));";
+
+					try{
+						$db_conn->conn->exec($q_string);
+						echo "adding table ".$table_name."<br>";
+					}catch(PDOException $ex)
+					{
+						echo "Could not create table $table_name: " .$ex->getMessage() ."<br>";
+					}
+				}
+				else
+				{
+					foreach($table->childNodes as $column)
+					{
+						if($column->nodeName!='#text' && $column->nodeName!='#comment')
+						{
+							$match=false;
+							for($i=0;$i<count($get_res);$i++)
+							{
+								if($get_res[$i]['column_name']==$column->nodeName)
+								{
+									$match=true;
+									break;
+								}
+							}
+							if($match==false)
+							{
+								echo "adding column ".$column->nodeName." to table $table_name<br>";
+								$alter_query="alter table $table_name add ".$column->nodeName." ".$column->getAttribute('type');
+
+								try{
+									$db_conn->conn->exec($alter_query);
+								}catch(PDOException $ex)
+								{
+									echo "Could not update table $table_name: " .$ex->getMessage() ."<br>";
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	$db_schema_xml=new \DOMDocument();
+	$db_schema_xml->load("../db/master_db_schema.xml");
+	$db_schema=$db_schema_xml->documentElement;
+	$info_conn=new db_connect('information_schema');
+
+	echo "<b>updating db re_master</b><br>";
+	update_tables("re_master",$info_conn,$db_schema);
+
+?>
