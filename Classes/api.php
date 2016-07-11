abaf09278b092db2d944875e9c44d931b5de0ebb
Ashish Goyal <ashish.19goyal@gmail.com>
2016-07-12 00:00:55 +0530

    Automated commit on 2016-07-12 00:00:55 IST

--- Classes/api.php
+++ Classes/api.php
@@ -12,14 +12,17 @@ include_once 'vDB.php';
 class api
 {
 	private static $instance;
+	private static $username;
+	private static $apiKey;
 	private static $table;
 	private static $requestType;
 	private static $data;
 	private static $row;
 	private static $indexes;
-	private static $apiKey;
-	private static $username;
 	private static $options;
+	private static $requiredFields;
+	private static $indexFields;
+	private static $refactorFields;
 
 	/**
 	 * Constructor
@@ -69,8 +72,8 @@ class api
 	*/
 	public static function authenticateRequest()
 	{
-		$query="select dbname,data_stores from api_key_mapping where api_key=? and username=? and status=?";
-		$values=array(self::$apiKey,self::$username,'active');
+		$query="select * from api_key_mapping where api_key=? and username=? and status=? and data_stores=? and request_types like ?";
+		$values=array(self::$apiKey,self::$username,'active',self::$table,"%".self::$requestType."%");
 
 		$vDB=new vDB(0);
 		$result=$vDB->dbSelect($query,$values);
@@ -82,17 +85,93 @@ class api
 
 		if(count($result)>0)
 		{
-			$response['description'] = 'no data store access';
-
-			$data_stores=$result[0]['data_stores'];
-			if(strpos($data_stores,self::$table)!==false)
-			{
-				$response['status'] = 'success';
-				$response['description'] = 'api access granted';
-				$response['dbname']=$result[0]['dbname'];
-			}
+			$response['status'] = 'success';
+			$response['description'] = 'api access granted';
+			$response['dbname']=$result[0]['dbname'];
+			self::$requiredFields=$result[0]['required_fields'];
+			self::$refactorFields=$result[0]['re_factoring'];
+			self::$indexFields=$result[0]['indexes'];
 		}
 		return $response;
+	}
+
+	/**
+	* This function adds required additional indexes to a request
+	*/
+	private static function inflateIndexes()
+	{
+		if(isset(self::$indexFields))
+		{
+			$additionalIndexes = json_decode(self::$indexFields,true);
+			if(is_array($additionalIndexes))
+			{
+				self::$indexes = array_merge(self::$indexes, $additionalIndexes);
+			}
+		}
+		return true;
+	}
+
+	/**
+	* This function adds validates required fields
+	*/
+	private static function validateRequiredFields($row)
+	{
+		if(isset(self::$requiredFields))
+		{
+			$requiredFields = json_decode(self::$requiredFields,true);
+			if(is_array($requiredFields))
+			{
+				foreach($requiredFields as $field)
+				{
+					$present=false;
+					foreach($row as $index)
+					{
+						if($field['index'] == $index['index'])
+						{
+							$present = true;
+							break;
+						}
+					}
+					if(!$present)
+					{
+						return false;
+					}
+				}
+			}
+		}
+		return true;
+	}
+
+	/**
+	* This function refactor fields
+	*/
+	private static function refactorFields($row)
+	{
+		if(isset(self::$refactorFields))
+		{
+			$refactorFields = json_decode(self::$refactorFields,true);
+			if(is_array($refactorFields))
+			{
+				foreach($refactorFields as $refactor)
+				{
+					$refactored=false;
+					foreach($row as $key => $index)
+					{
+						if($refactor['index'] == $index['index'])
+						{
+							$row[$key] = $refactor + $index;
+							$refactored=true;
+							break;
+						}
+					}
+					if(!$refactored)
+					{
+						$row[]=$refactor;
+					}
+				}
+			}
+		}
+		return $row;
 	}
 
 	/**
@@ -108,16 +187,16 @@ class api
 			case 'delete': return self::delete($dbname);
 
 			default: return array(
-						'status' => 'error',
-						'description' => 'request type not supported'
-					);
+				'status' => 'error',
+				'description' => 'request type not supported'
+			);
 		}
 	}
 
 	/**
 	* This function executes a get request
 	*/
-	public static function get($dbname)
+	private static function get($dbname)
 	{
 		$vDB=new vDB($dbname);
 		$vDB->setTable(self::$table);
@@ -126,7 +205,7 @@ class api
 			'status' => 'success',
 			'data_store' => self::$table
 		);
-
+		self::inflateIndexes();
 		$result = array_merge($result, $vDB->vRead(self::$indexes,self::$options));
 		return $result;
 	}
@@ -134,7 +213,7 @@ class api
 	/**
 	* This function executes a put request
 	*/
-	public static function put($dbname)
+	private static function put($dbname)
 	{
 		$vDB=new vDB($dbname);
 		$vDB->setTable(self::$table);
@@ -147,16 +226,15 @@ class api
 
 		if(count(self::$row)>0)
 		{
-			if($vDB->vPut(self::$row))
+			if(self::validateRequiredFields(self::$row))
 			{
-				$result['rows'][] = array(
-					'status' => 'success',
-					'row' => self::$row
-				);
+				self::$row = self::refactorFields(self::$row);
+				return $vDB->vPut(self::$row);
 			}
-			else {
+			else{
 				$result['rows'][] = array(
 					'status' => 'error',
+					'description' => 'required indexes missing',
 					'row' => self::$row
 				);
 			}
@@ -165,16 +243,15 @@ class api
 		{
 			foreach(self::$data as $row)
 			{
-				if($vDB->vPut($row))
+				if(self::validateRequiredFields($row))
 				{
-					$result['rows'][] = array(
-						'status' => 'success',
-						'row' => $row
-					);
+					$row = self::refactorFields($row);
+					return $vDB->vPut($row);
 				}
-				else {
+				else{
 					$result['rows'][] = array(
 						'status' => 'error',
+						'description' => 'required indexes missing',
 						'row' => $row
 					);
 				}
@@ -186,7 +263,7 @@ class api
 	/**
 	* This function executes a post request
 	*/
-	public static function post($dbname)
+	private static function post($dbname)
 	{
 		$vDB=new vDB($dbname);
 		$vDB->setTable(self::$table);
@@ -199,16 +276,15 @@ class api
 
 		if(count(self::$row)>0)
 		{
-			if($vDB->vCreate(self::$row))
+			if(self::validateRequiredFields(self::$row))
 			{
-				$result['rows'][] = array(
-					'status' => 'success',
-					'row' => self::$row
-				);
+				self::$row = self::refactorFields(self::$row);
+				return $vDB->vCreate(self::$row);
 			}
-			else {
+			else{
 				$result['rows'][] = array(
 					'status' => 'error',
+					'description' => 'required indexes missing',
 					'row' => self::$row
 				);
 			}
@@ -217,16 +293,15 @@ class api
 		{
 			foreach(self::$data as $row)
 			{
-				if($vDB->vCreate($row))
+				if(self::validateRequiredFields($row))
 				{
-					$result['rows'][] = array(
-						'status' => 'success',
-						'row' => $row
-					);
+					$row = self::refactorFields($row);
+					return $vDB->vCreate($row);
 				}
-				else {
+				else{
 					$result['rows'][] = array(
 						'status' => 'error',
+						'description' => 'required indexes missing',
 						'row' => $row
 					);
 				}
@@ -238,7 +313,7 @@ class api
 	/**
 	* This function executes a delete request
 	*/
-	public static function delete($dbname)
+	private static function delete($dbname)
 	{
 		$vDB=new vDB($dbname);
 		$vDB->setTable(self::$table);
@@ -247,7 +322,7 @@ class api
 			'status' => 'success',
 			'data_store' => self::$table
 		);
-
+		self::inflateIndexes();
 		if($vDB->vDelete(self::$indexes))
 		return $result;
 	}
